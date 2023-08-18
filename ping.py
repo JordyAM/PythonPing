fe85d42bb34eb70b3a3727eb1fb9c4e1799b4dc6
JordyAM <JalexanderMcLean@Gmail.com>
2023-08-17 23:37:11 -0500

    adding frontend

--- ping.py
+++ ping.py
@@ -2,12 +2,13 @@
 
 import subprocess
 import platform
+import cgi
 
 def ping(host):
     param = '-n' if platform.system().lower() == 'windows' else '-c'
     command = ['ping', param, '1', host]
     return subprocess.call(command)
 
-host = input('enter website:')
+host_element = 
 
 print(ping(host))
